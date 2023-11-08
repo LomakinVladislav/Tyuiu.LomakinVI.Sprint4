a0331e3303220ef9000d590248c2f86a9c2131e9
Tyuiu.LomskinVI@outlook.com <winr2005@gmail.com>
2023-11-08 18:48:19 +0500

    Task5

---
+++ Tyuiu.LomakinVI.Sprint4.Task5.V22.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LomakinVI.Sprint4.Task5.V22.Lib;
+
+namespace Tyuiu.LomakinVI.Sprint4.Task5.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] testArray = new int[,] { { 0, -5, 2, -7 }, { 5, -1, 3, 0 }, { 7, -2, -11, 1 } };
+
+            Assert.AreEqual(5, ds.Calculate(testArray));
+        }
+    }
+}
---
+++ Tyuiu.LomakinVI.Sprint4.Task5.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LomakinVI.Sprint4.Task5.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LomakinVI.Sprint4.Task5.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87ff2749-933b-41f1-b395-097ee230a60e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
